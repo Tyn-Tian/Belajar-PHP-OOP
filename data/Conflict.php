fcedc7920f34579f8e6fa798c2db89aba0f3c1c4
lukxnut <kristian2015ok@gmail.com>
2024-04-05 11:59:55 +0700

    Add: Import

--- data/Conflict.php
+++ data/Conflict.php
@@ -4,11 +4,18 @@ namespace Data\One {
     class Conflict
     {
     }
+
+    class Sample
+    {
+    }
+
+    class Dummy
+    {
+    }
 }
 
 namespace Data\Two {
     class Conflict
     {
-        
     }
 }
