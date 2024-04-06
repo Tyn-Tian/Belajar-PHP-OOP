9903ba5d635a940129a6a4af5d5293e7c2221471
lukxnut <kristian2015ok@gmail.com>
2024-04-06 14:18:25 +0700

    Add: Abstract Class

---
+++ AbstractClass.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once "data/Location.php";
+
+use Data\{Location, City, Province, Country};
+
+// $location = new Location(); Error
+
+$city = new City();
+$Province = new Province();
+$Country = new Country();
