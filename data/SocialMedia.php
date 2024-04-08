7e155a81a7168b76b2aacc2782f69d15a7222a11
lukxnut <kristian2015ok@gmail.com>
2024-04-08 14:55:43 +0700

    Add: Final Function

--- data/SocialMedia.php
+++ data/SocialMedia.php
@@ -3,11 +3,20 @@
 class SocialMedia 
 {
     public string $name;
+
+    final public function login(string $username, string $password): bool
+    {
+        return true;
+    }
 }
 
 final class Facebook extends SocialMedia
 {
-
+    // Error
+    // public function login(string $username, string $password): bool 
+    // {
+    //     return false;
+    // }
 }
 
 // Error
