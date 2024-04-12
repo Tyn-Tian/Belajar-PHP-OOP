39607a165dfd7d5464c69f17abe7739b6cae18c3
lukxnut <kristian2015ok@gmail.com>
2024-04-12 14:36:26 +0700

    Add: PropertiesOverloading.php

---
+++ PropertiesOverloading.php
@@ -0,0 +1,48 @@
+<?php
+
+class Zero
+{
+    private array $properties = [];
+
+    public function __get($name)
+    {
+        return $this->properties[$name];
+    }
+
+    public function __set($name, $value)
+    {
+        $this->properties[$name] = $value;
+    }
+
+    public function __isset($name): bool
+    {
+        return isset($this->properties[$name]);
+    }
+
+    public function __unset($name)
+    {
+        unset($this->properties[$name]);
+    }
+
+    public function __call($name, $arguments)
+    {
+        $join = join(",", $arguments);
+        echo "Call function $name with arguments $join" . PHP_EOL;
+    }
+
+    public static function __callStatic($name, $arguments)
+    {
+        $join = join(",", $arguments);
+        echo "Call static function $name with arguments $join" . PHP_EOL;
+    }
+}
+
+$zero = new Zero();
+$zero->firstName = "Chris";
+$zero->lastName = "Tian";
+
+echo "First Name : $zero->firstName" . PHP_EOL;
+echo "Last Name : $zero->lastName" . PHP_EOL;
+
+$zero->sayHello("Chris", "Tian");
+Zero::sayHello("Chris", "Tian");
